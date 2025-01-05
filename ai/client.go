1c1b3b39351f8a44e79833cae638aec9f64b9c93
ALev <me@alev.pro>
2025-01-05 17:58:53 +0100

    Add model type setting ability

--- ai/client.go
+++ ai/client.go
@@ -4,9 +4,19 @@ type MessageType string
 
 const (
 	MessageTypeUser      MessageType = "user"
+	MessageTypeSystem    MessageType = "system"
 	MessageTypeAi        MessageType = "ai"
 	MessageTypeTool      MessageType = "tool"
 	MessageTypeUndefined MessageType = "undefined"
+)
+
+type ModelSize string
+
+const (
+	ModelS  ModelSize = "s"
+	ModelM  ModelSize = "m"
+	ModelL  ModelSize = "l"
+	ModelXL ModelSize = "xl"
 )
 
 type Message interface {
@@ -16,12 +26,14 @@ type Message interface {
 
 type Dialog interface {
 	AppendUserMessage(message string)
+	AppendSystemMessage(message string)
 	GetMessages() []Message
+
+	SetModelSize(size ModelSize) error
+	SetTools(tools []Tool) error
 }
 
 type Client interface {
 	NewDialog() Dialog
 	Query(query string, dialog Dialog) error
-
-	SetTools(tools []Tool)
 }
